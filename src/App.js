7ce4995a5ab73b0607a0a45ff94eef821c50c292
Rishabh Singh <ris8645208@gmail.com>
2021-09-09 20:33:43 +0530

    chore: add react-router pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import Home from "./app/page/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route  path="/home" component={Home} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
