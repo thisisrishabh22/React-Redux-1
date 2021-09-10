4ea3bcf8d0c03eaf88c5d07fd02796312d52d5d1
Rishabh Singh <ris8645208@gmail.com>
2021-09-10 18:12:15 +0530

    chore: add card component to home

--- src/app/page/Home.jsx
+++ src/app/page/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Card from '../components/Card';
 import Navbar from '../components/Navbar'
 
 const Home = () => {
+    const state = useSelector(state => { if (state) return (state) });
+    console.log(state)
     return (
         <>
             <Navbar />
+            <Card />
         </>
     )
 }
