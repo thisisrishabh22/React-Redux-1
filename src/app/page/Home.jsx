1e54c326853ba9287283e979615552ea42982e41
Rishabh Singh <ris8645208@gmail.com>
2021-09-12 13:53:36 +0530

    chore: add post page

--- src/app/page/Home.jsx
+++ src/app/page/Home.jsx
@@ -1,6 +1,7 @@
 import { Box, Grid } from '@material-ui/core';
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Card from '../components/Card';
 import Navbar from '../components/Navbar'
 
@@ -12,7 +13,7 @@ const Home = () => {
             <Navbar />
             <Grid container>
                 {state && state.posts.map((post) => (
-                    <Grid item md={6} sm={12} ><Box style={{margin:10}}><Card  post={post} /></Box></Grid>
+                    <Grid item md={6} sm={6} xs={12} ><Box style={{ margin: 10 }}><Link to="/about"><Card post={post} /></Link></Box></Grid>
                 ))}
             </Grid>
         </>
---
+++ src/app/page/Posts.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../components/Navbar'
+
+const Posts = () => {
+    return (
+        <>
+            <Navbar />
+            
+        </>
+    )
+}
+
+export default Posts;
